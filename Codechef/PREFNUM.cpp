e5af4d89e6cd0922bb3e2f753d37f5f04f86ee68
Deepanjan Kowar <deepanjan.kowar@cleartax.in>
2018-12-25 11:08:43 +0530

    Create PREFNUM.cpp

---
+++ Codechef/PREFNUM.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void exec() {
+	long long x,y,a,f=0,lim,curr;
+	lim=pow(10,18);
+	cin>>x>>y;
+	for(long long i=1;!f;i*=10) {
+	    	a = i*x;
+		if(a>lim/10) f=1;
+        curr=a%y;
+        if(curr==0)  {
+		    cout<<a<<endl;
+		    return;
+		}
+		if(y-curr < i) {
+			a=a+y-curr;
+			if(a>0 and a<=lim) {
+				cout<<a<<endl;
+				return;
+			}
+		} 
+	}
+	cout<<"-1\n";
+	return;
+}
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--) {
+        exec();
+    }
+}
